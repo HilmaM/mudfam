0ec2eec5ef267c6b42361f0a6103526140a78711
hilmam <hazelman@live.com>
2020-09-03 09:32:55 +0200

    Error resolution

--- src/index.jsx
+++ src/index.jsx
@@ -2,31 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { history } from './_helpers';
 import { accountService } from './_services';
+import { App } from './App/App';
 
 import './styles.less';
 
-import { App } from './App';
 import { Router } from 'react-router-dom';
 
-function startApp() { 
-  render(
-    <Router history={history}>
-      <App />
-    </Router>,
-    document.getElementById('app')
-  );
-};
-
-startApp();
-
-/**
- * Remember to uncomment the line below from a live environment 
- *
 // attempt silent token refresh before startup
-//accountService.refreshToken().finally(startApp);
+accountService.refreshToken().finally(startApp);
 
 function startApp() { 
-  return import(/* webpackChunkName: "lodash" * 'lodash').then(({ default: _ }) => {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
     render(
       <Router history={history}>
         <App />
@@ -38,4 +24,4 @@ function startApp() {
 
 startApp().then(component => {
   document.body.appendChild(component);
-});*/
+});
